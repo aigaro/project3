13bc2258e7e0e38e813f17d16e5ad407c56f997c
Apple Inc <g024080@arrahhidoonbmsise.local>
2017-01-18 17:19:59 +0200

    First project done!

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -11,8 +11,15 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Viewing picture \(selectedImage!)"
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +29,16 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false 
+    }
+
 
     /*
     // MARK: - Navigation
